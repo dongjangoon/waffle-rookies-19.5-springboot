08a24dfee6477b65db62d1bbee79b467a934b8f6
numnum97 <numnum97@naver.com>
2021-09-06 01:42:48 +0900

    Feat: 유저 자기 자신 정보 확인

--- seminar/src/main/kotlin/com/wafflestudio/seminar/domain/user/api/UserController.kt
+++ seminar/src/main/kotlin/com/wafflestudio/seminar/domain/user/api/UserController.kt
@@ -1,9 +1,11 @@
 package com.wafflestudio.seminar.domain.user.api
 
 import com.wafflestudio.seminar.domain.user.dto.UserDto
+import com.wafflestudio.seminar.domain.user.exception.UserNotFoundException
 import com.wafflestudio.seminar.domain.user.model.User
 import com.wafflestudio.seminar.domain.user.service.UserService
 import org.modelmapper.ModelMapper
+import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.*
 import javax.validation.Valid
 
@@ -23,8 +25,21 @@ class UserController(
         @ModelAttribute @Valid body: UserDto.CreateRequest
     ): UserDto.Response {
         val newUser = modelMapper.map(body, User::class.java)
-        val response = modelMapper.map(userService.saveUser(newUser), UserDto.Response::class.java)
-        return response
+        return modelMapper.map(userService.saveUser(newUser), UserDto.Response::class.java)
+    }
+
+    /**
+     * 서비스 단에서 넘겨준 user 로 responseBody 구성, null 이면 에러 발생
+     */
+    @GetMapping("me/")
+    fun getSelfInfo(@RequestHeader("User-Id") id: Long): ResponseEntity<UserDto.Response> {
+        return try {
+            val user = userService.getUserById(id)
+            val responseBody = modelMapper.map(user, UserDto.Response::class.java)
+            ResponseEntity.ok(responseBody)
+        } catch (e: UserNotFoundException) {
+            ResponseEntity.notFound().build()
+        }
     }
 
 }
