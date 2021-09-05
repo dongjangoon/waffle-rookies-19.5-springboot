1139ad21f3223179110bfae2d78cf55fd09d57ee
numnum97 <numnum97@naver.com>
2021-09-06 01:06:56 +0900

    Fix constructor in User.kt

--- seminar/src/main/kotlin/com/wafflestudio/seminar/domain/user/model/User.kt
+++ seminar/src/main/kotlin/com/wafflestudio/seminar/domain/user/model/User.kt
@@ -9,18 +9,18 @@ import javax.validation.constraints.Email
 import javax.validation.constraints.NotBlank
 
 @Entity
-class User {
+class User (
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    var id: Long? = null
+    var id: Long? = null,
 
     @NotNull
     @NotBlank
-    var name: String? = null
+    var name: String? = null,
 
     @NotNull
     @NotBlank
     @Email
-    var email: String? = null
-}
+    var email: String? = null,
+)
