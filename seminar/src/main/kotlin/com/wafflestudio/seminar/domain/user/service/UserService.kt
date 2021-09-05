328b0da03fed6b288901f998a813cf254f1e7a9f
numnum97 <numnum97@naver.com>
2021-09-06 01:42:48 +0900

    Feat: 유저 자기 자신 정보 확인

--- seminar/src/main/kotlin/com/wafflestudio/seminar/domain/user/service/UserService.kt
+++ seminar/src/main/kotlin/com/wafflestudio/seminar/domain/user/service/UserService.kt
@@ -1,8 +1,10 @@
 package com.wafflestudio.seminar.domain.user.service
 
 import com.wafflestudio.seminar.domain.user.dto.UserDto
+import com.wafflestudio.seminar.domain.user.exception.UserNotFoundException
 import com.wafflestudio.seminar.domain.user.model.User
 import com.wafflestudio.seminar.domain.user.repository.UserRepository
+import org.springframework.data.repository.findByIdOrNull
 import org.springframework.stereotype.Service
 
 @Service
@@ -16,6 +18,14 @@ class UserService(
      */
     fun saveUser(user: User): User? {
         return userRepository.save(user)
+    }
+
+    /**
+     * findByIdOrNull 의 반환형이 Optional 이므로 null 검사를 하고 아닐시
+     * UserNotFoundException throw
+     */
+    fun getUserById(id: Long): User? {
+        return userRepository.findByIdOrNull(id) ?: throw UserNotFoundException()
     }
 
 }
