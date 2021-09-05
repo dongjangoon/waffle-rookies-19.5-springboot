effb5e2c16b71394d8e4f5acd3b11b6dcec520c8
numnum97 <numnum97@naver.com>
2021-09-05 22:58:02 +0900

    Feat: Add User

---
+++ seminar/src/main/kotlin/com/wafflestudio/seminar/domain/user/model/User.kt
@@ -0,0 +1,26 @@
+package com.wafflestudio.seminar.domain.user.model
+
+import com.sun.istack.NotNull
+import javax.persistence.Entity
+import javax.persistence.GeneratedValue
+import javax.persistence.GenerationType
+import javax.persistence.Id
+import javax.validation.constraints.Email
+import javax.validation.constraints.NotBlank
+
+@Entity
+class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    var id: Long? = null
+
+    @NotNull
+    @NotBlank
+    var name: String? = null
+
+    @NotNull
+    @NotBlank
+    @Email
+    var email: String? = null
+}
