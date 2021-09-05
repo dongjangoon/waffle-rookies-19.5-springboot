9fbf0f602f2b8e9c86dc8255e4cfc5170ec7dd94
numnum97 <numnum97@naver.com>
2021-09-06 01:07:31 +0900

    Add test users

--- seminar/src/main/kotlin/com/wafflestudio/seminar/DataLoader.kt
+++ seminar/src/main/kotlin/com/wafflestudio/seminar/DataLoader.kt
@@ -4,6 +4,8 @@ import com.wafflestudio.seminar.domain.os.model.OperatingSystem
 import com.wafflestudio.seminar.domain.os.repository.OperatingSystemRepository
 import com.wafflestudio.seminar.domain.survey.model.SurveyResponse
 import com.wafflestudio.seminar.domain.survey.repository.SurveyResponseRepository
+import com.wafflestudio.seminar.domain.user.model.User
+import com.wafflestudio.seminar.domain.user.repository.UserRepository
 import org.springframework.boot.ApplicationArguments
 import org.springframework.boot.ApplicationRunner
 import org.springframework.core.io.ClassPathResource
@@ -18,6 +20,7 @@ import java.time.format.DateTimeFormatter
 class DataLoader(
     private val operatingSystemRepository: OperatingSystemRepository,
     private val surveyResponseRepository: SurveyResponseRepository,
+    private val userRepository: UserRepository,
 ) : ApplicationRunner {
     // 어플리케이션 동작 시 실행
     override fun run(args: ApplicationArguments) {
@@ -44,5 +47,16 @@ class DataLoader(
                 surveyResponseRepository.save(newSurveyResponse)
             }
         }
+
+        // 테스트용 유저 삽입
+        val user1 = User(name = "동숙이", email = "dongsuk@suk.com")
+        val user2 = User(name = "현숙이", email = "hyunsuk@suk.com")
+        val user3 = User(name = "철숙이", email = "chulsuk@suk.com")
+        val user4 = User(name = "대숙이", email = "daesuk@suk.com")
+        userRepository.save(user1)
+        userRepository.save(user2)
+        userRepository.save(user3)
+        userRepository.save(user4)
+
     }
 }
