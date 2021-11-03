9841b052fdb5a6c10f957dd9a3b7ed60a8e525ef
numnum97 <numnum97@naver.com>
2021-11-03 22:50:04 +0900

    .

--- seminar/src/test/kotlin/com/wafflestudio/seminar/domain/user/service/UserServiceTest.kt
+++ seminar/src/test/kotlin/com/wafflestudio/seminar/domain/user/service/UserServiceTest.kt
@@ -1,14 +1,22 @@
 package com.wafflestudio.seminar.domain.user.service
 
+import com.wafflestudio.seminar.domain.user.dto.UserDto
+import com.wafflestudio.seminar.domain.user.exception.UserAlreadyExistsException
 import com.wafflestudio.seminar.domain.user.model.User
+import com.wafflestudio.seminar.domain.user.repository.ParticipantProfileRepository
 import com.wafflestudio.seminar.domain.user.repository.UserRepository
 import org.junit.jupiter.api.Test
 
 import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.assertThrows
+import org.mockito.Mock
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.boot.test.mock.mockito.MockBean
-import org.springframework.test.web.servlet.ResultActionsDsl
+import org.springframework.security.crypto.password.PasswordEncoder
+import org.springframework.test.context.TestConstructor
 import javax.transaction.Transactional
 
 @AutoConfigureMockMvc
@@ -16,20 +24,39 @@ import javax.transaction.Transactional
 internal class UserServiceTest {
 
     @MockBean
-    lateinit var repository: UserRepository
+    private lateinit var userRepository: UserRepository
 
     @MockBean
-    lateinit var service: UserService
+    private lateinit var participantProfileRepository: ParticipantProfileRepository
 
-    @Test
-    @Transactional
-    fun `회원가입 성공`() {
+    @MockBean
+    private lateinit var passwordEncoder: PasswordEncoder
 
+    @Autowired
+    private lateinit var userService: UserService
+
+    @BeforeEach
+    internal fun setUp() {
+        userService = UserService(userRepository, participantProfileRepository, passwordEncoder)
     }
 
     @Test
     @Transactional
-    fun `회원가입 중복 이메일 검증`() {
+    fun `회원가입에 성공한다`() {
+        val user = signupAsInstructorUser("waffle")
+
+        assertEquals(user.email, "waffle@snu.ac.kr")
+        assertEquals(user.roles, "instructor")
+    }
+
+    @Test
+    @Transactional
+    fun `이미 가입된 메일이라면 예외가 발생한다`() {
+        val user = signupAsInstructorUser("waffle")
+
+        assertThrows<UserAlreadyExistsException> {
+            signupAsInstructorUser("waffle")
+        }
 
     }
 
@@ -42,21 +69,14 @@ internal class UserServiceTest {
     }
 
     private fun signupAsInstructorUser(name: String): User {
-        val body =
-            """
-                {
-                    "name": "$name",
-                    "email": "${name}@snu.ac.kr",
-                    "password": "$name",
-                    "role": "instructor",
-                    "company": "google",
-                    "year": "1",
-                }
-            """.trimIndent()
-        return signup(body)
-    }
-
-    private fun signup(body: UserDto.SignupRequest): User {
-        return service.signup(body)
+        val body = UserDto.SignupRequest(
+            name,
+            "${name}@snu.ac.kr",
+            name,
+            "instructor",
+            company = "google",
+            year = 1
+        )
+        return userService.signup(body)
     }
 }
