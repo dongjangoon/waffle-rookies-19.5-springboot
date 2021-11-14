8775453e58c933a581942942a90230dae9f40b81
numnum97 <numnum97@naver.com>
2021-11-14 16:51:28 +0000

    Update deploy.sh

--- seminar/scripts/deploy.sh
+++ seminar/scripts/deploy.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-cd /home/ec2-user/waffle-rookies-19.5-springboot/seminar
+REPOSITORY=/home/ec2-user/
+
+cd $REPOSITORY/waffle-rookies-19.5-springboot/
 
 echo "> Git Pull"
 
@@ -10,6 +12,28 @@ echo "> Build"
 
 ./gradlew bootJar
 
-echo "> Execute jar file"
+echo "> Build file copy"
 
-nohup java -jar -Dspring.profiles.active=prod build/libs/seminar-0.0.1-SNAPSHOT.jar
+cp ./seminar/build/libs/*.jar $REPOSITORY/
+
+echo "> Application pid check"
+
+CURRENT_PID=$(pgrep -f seminar)
+
+echo "$CURRENT_PID"
+
+if [ -z $CURRENT_PID ]; then
+	echo "> 현재 구동중인 애플리케이션이 없으므로 종료하지 않습니다."
+else
+	echo "> kill -2 $CURRENT_PID"
+	kill -2 $CURRENT_PID
+	sleep 5
+fi
+
+echo "> 새 애플리케이션 배포"
+
+JAR_NAME=$(ls $REPOSITORY | grep 'seminar' | tail -n 1)
+
+echo "> JAR Name: $JAR_NAME"
+
+nohup java -jar -Dspring.profiles.active=prod $REPOSITORY/$JAR_NAME &
